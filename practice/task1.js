1fb55485932b96da857730ad34be9124d24c3403
KripashreeBhat <kripashree.bhat@robosoftin.com>
2022-09-02 11:10:47 +0530

    my initial commit

---
+++ practice/task1.js
@@ -0,0 +1,13 @@
+
+    // let ques=prompt('what is the "offical"name of JavaScript?','');
+    // if(ques=='ECMAScript')
+    // {
+    console.log("right");
+// }
+
+//     else{
+//         console.log("you dont know?");
+
+//     }
+    
+
