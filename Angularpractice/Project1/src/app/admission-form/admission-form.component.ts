9ebb169dd066f33b69efa09e82916222fb59d66d
KripashreeBhat <kripashree.bhat@robosoftin.com>
2022-10-11 16:22:18 +0530

    updated template driven form -admission

--- Angularpractice/Project1/src/app/admission-form/admission-form.component.ts
+++ Angularpractice/Project1/src/app/admission-form/admission-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { HostListener } from '@angular/core';
+import { Register } from '../register';
 
 @Component({
   selector: 'app-admission-form',
@@ -9,20 +10,28 @@ import { HostListener } from '@angular/core';
 })
 export class AdmissionFormComponent implements OnInit {
 
-  firstname: string="";
-  middlename: string="";
-  lastname: string="";
-  gender: boolean=false;
-  fathername: string="";
-  mothername: string="";
-  grade: string="";
-  gradee: string="";
-  board: string="";
-  boardd: string="";
-  domain:string="";
-  terms:boolean=false;
-  phone: string="";
-  mail: string="";
+  // firstname: string="";
+  // middlename: string="";
+  // lastname: string="";
+  // gender: boolean=false;
+  // fathername: string="";
+  // mothername: string="";
+  // grade: string="jbb";
+  // gradee: string="";
+  // board: string="";
+  // boardd: string="";
+  // domain:string="";
+  // terms:boolean=false;
+  // phone: string="";
+  // mail: string="";
+  topics=['ComputerScience Engineering',
+  'Mechanical Engineering',
+ 'AI ML Engineering',
+  'Electronics Engineering',
+  'Civil Engineering'];
+  registerModel = new Register('Kripa','','Bhat','female','KV Bhat',
+  'Roopa Bhat',7417428078,'kribhat@gmail.com','9.6cgpa','96%','ComputerScience Engineering',true);
+
   constructor() { }
   @HostListener('submit') submit(){
     alert('Form submitted');
---
+++ Angularpractice/Project1/src/app/register.ts
@@ -0,0 +1,19 @@
+export class Register {
+constructor(
+    public firstname: string,
+    public middlename: string,
+    public lastname: string,
+    public gender: string,
+    public fathername: string,
+    public mothername: string,
+    public phone: number,
+    public mail: string,
+    public grade: string,
+    public gradee: string,
+    public domain:string,
+    public terms:boolean,
+
+){}
+    
+}
+ 
